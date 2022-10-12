99c5e5a66a17481726ce00cac8eb6788b5cb6174
bayram98 <bayramguwanjow@gmail.com>
2022-10-12 18:40:01 +0900

    chore: configuration update

--- jest.setup.js
+++ jest.setup.js
@@ -4,10 +4,12 @@ const ethers = require('ethers')
 dotenv.config()
 
 // Baobab 
-// const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+const wallet1 = ethers.Wallet.fromMnemonic(process.env.MNEMONIC1)
 
-const wallet = new ethers.Wallet(process.env.privateKey)
-const wallet1 = new ethers.Wallet(process.env.privateKey1)
+// Localhost
+//const wallet = new ethers.Wallet(process.env.privateKey)
+//const wallet1 = new ethers.Wallet(process.env.privateKey1)
 
 global.Wallet = wallet
 global.Wallet1 = wallet1
