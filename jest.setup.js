068ed9404118d29c4714cc35a3132aff65d42e19
bayram98 <bayramguwanjow@gmail.com>
2022-10-13 17:42:54 +0900

    chore: fix variable naming

--- jest.setup.js
+++ jest.setup.js
@@ -3,13 +3,15 @@ const ethers = require('ethers')
 
 dotenv.config()
 
-// Baobab 
-const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-const wallet1 = ethers.Wallet.fromMnemonic(process.env.MNEMONIC1)
-
-// Localhost
-//const wallet = new ethers.Wallet(process.env.privateKey)
-//const wallet1 = new ethers.Wallet(process.env.privateKey1)
-
-global.Wallet = wallet
+let wallet0, wallet1
+if (process.env.RUNNING_NETWORK == 'baobab') {
+  // Baobab
+  wallet0 = ethers.Wallet.fromMnemonic(process.env.MNEMONIC0)
+  wallet1 = ethers.Wallet.fromMnemonic(process.env.MNEMONIC1)
+} else {
+  // klaytnDevSandBox Localhost
+  wallet0 = new ethers.Wallet(process.env.PRIVATEKEY0)
+  wallet1 = new ethers.Wallet(process.env.PRIVATEKEY1)
+}
+global.Wallet0 = wallet0
 global.Wallet1 = wallet1
--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,14 @@
 export const SETTINGS = {
   localhost: {
-    // RPC_ENDPOINT:'http://127.0.0.1:8545'
-    RPC_ENDPOINT:'http://0.0.0.0:8551',
-    SBT_CONTRACT_ADDRESS : '0x7fB8250cBaE61dFdfabeA10D3b7C9b6A90D47819'
+    RPC_ENDPOINT: 'http://0.0.0.0:8551',
+    SBT_TEST_CONTRACT_ADDRESS: '0x7fB8250cBaE61dFdfabeA10D3b7C9b6A90D47819'
   },
   baobab: {
     RPC_ENDPOINT: 'https://public-node-api.klaytnapi.com/v1/baobab',
-    SBT_CONTRACT_ADDRESS : '0x7fB8250cBaE61dFdfabeA10D3b7C9b6A90D47819'
+    SBT_TEST_CONTRACT_ADDRESS: '0x7fB8250cBaE61dFdfabeA10D3b7C9b6A90D47819'
   },
   cypress: {
     RPC_ENDPOINT: 'https://public-node-api.klaytnapi.com/v1/cypress',
-    SBT_CONTRACT_ADDRESS : '0x7fB8250cBaE61dFdfabeA10D3b7C9b6A90D47819'
+    SBT_TEST_CONTRACT_ADDRESS: ''
   }
 }
