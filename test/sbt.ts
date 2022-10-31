9d3fcb389bf0413991840b0132339d20792cfba5
Martin Kersner <martin@bisonai.com>
2022-10-31 19:22:08 +0900

    chore: Typo

--- test/sbt.ts
+++ test/sbt.ts
@@ -33,8 +33,8 @@ describe('SBT', () => {
     const network = process.env.TEST_NETWORK || Network.localhost
     if (network == Network.localhost) {
       // We recommend using ganache with -d option to generate
-      // accounts in a deterministic way. Following accounts prefunded
-      // by ganache.
+      // accounts in a deterministic way. Following accounts are
+      // prefunded by ganache.
 
       // account 0
       ACCOUNTS.privateKey0 = '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
